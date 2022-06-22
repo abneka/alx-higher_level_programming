a20b5866895fb49c174cea4d56de8613d5e41697
abneka <anekahiwot@gmail.com>
2022-06-22 02:37:13 +0000

    python:

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Create a class Square that is define based ont 0-square.py
+"""
+
+
+class Square:
+    """ This class has a private instance called size """
+    def __init__(self, size):
+        self.__size = size
